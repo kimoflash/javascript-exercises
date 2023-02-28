2bdadd229c57ed958a836ed1d7302f1313a134c5
Kim Oflash <105842823+kimoflash@users.noreply.github.com>
2023-02-28 17:41:54 +0800

    Function repeats a number of times and returns an error message if number is less than zero

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,11 +1,17 @@
 const repeatString = function(string, number) {
     let sumOfString = '';
 
-    for (i = 1; i <= number; i++) {
-        sumOfString += string;
+    if (number > 0) {
+        for (i = 1; i <= number; i++) {
+            sumOfString += string;
+        }
+    
+        return sumOfString;
+    } else if (number === 0) {
+        return sumOfString;
+    } else {
+        return "ERROR";
     }
-
-    return sumOfString;
 };
 
 // Do not edit below this line
