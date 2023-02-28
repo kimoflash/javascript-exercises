e82bf26018db4ade71843d70864a9f1b37db3328
Kim Oflash <105842823+kimoflash@users.noreply.github.com>
2023-02-28 17:33:56 +0800

    Funtion repeats string a number of times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(string, number) {
+    let sumOfString = '';
 
+    for (i = 1; i <= number; i++) {
+        sumOfString += string;
+    }
+
+    return sumOfString;
 };
 
 // Do not edit below this line
