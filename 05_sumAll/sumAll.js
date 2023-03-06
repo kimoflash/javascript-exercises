9a814f4858a7c8f563555d6eefb87c364b7d3df6
Kim Oflash <105842823+kimoflash@users.noreply.github.com>
2023-03-06 14:56:57 +0800

    Function accepts two numbers and adds all the numbers between them including the two numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+   if (isNaN(num1) || isNaN(num2)) {
+        return "ERROR";
+    } else if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    } else if (typeof(num1) == 'string' || typeof(num2) == 'string') {
+        return "ERROR";
+    } else {
+        let sum = 0;
 
+        if (num1 < num2) {
+            for (let i = num1; i <= num2; i++) {
+                sum += i;
+            }
+        } else {
+            for (let i = num2; i <= num1; i++) {
+                sum += i;
+            }
+        }
+
+        return sum;
+    }
 };
 
 // Do not edit below this line
